ba53893ac9887e2e96676f34ac560f86e4417c14
senivlm <91743906+senivlm@users.noreply.github.com>
2022-06-07 14:58:30 +0300

    Update Program.cs

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 namespace Assignment4
 {
     internal class Program
-    {
+    {// Ваш номер 13.
         static void Main(string[] args)
         {
             Vector arr = new Vector(25);
             arr.InitShuffle();
             Console.WriteLine(arr.ToString());
+            // краще зовнішнй enum
             arr.QuickSort(Vector.PivotType.Last, 0, 15);
             Console.WriteLine(arr.ToString());
 
